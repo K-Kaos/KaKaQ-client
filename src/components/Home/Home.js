6ff3bf39fdd9c55aca4795e257a350aad5b92617
서빈이 <bommy00@naver.com>
2023-04-14 23:09:50 +0900

    feat: Link to button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/Projects/home.png";
 import Particle from "../Particle";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        <Particle />
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">     
@@ -19,13 +19,13 @@ function Home() {
                       </span></h1>
                       <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Share your opinion with KaKaQ and meet your friends.</p>
                       <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                          <a href="/test" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                              Get started
-                              <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                          </a>
-                          <a href="/guide" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                              Learn more
-                          </a>  
+                        <Link to="/test" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" style={{ textDecoration: "none" }}>                              
+                          Get started
+                          <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </Link>
+                        <Link to="/guide" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800" style={{ textDecoration: "none" }}>
+                          Learn more
+                        </Link> 
                       </div>
                   </div>
               </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,6 @@ function NavBar() {
   }
   window.addEventListener("scroll", scrollHandler);
   return (
-
     <nav class="bg-warning dark:bg-gray-900 ">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" class="flex items-center" style={{ textDecoration: "none" }}>
@@ -41,10 +40,9 @@ function NavBar() {
           <span class="text-black self-center text-4xl font-semibold whitespace-nowrap dark:text-white focus:outline-none" style={{ textDecoration: "none" }}>KaKaQ</span>
         </a>
         <div class="flex md:order-2 text-2xl">
-          <button type="button" onclick="location.href='../src/components/Login/Login.js'" href="/login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+          <Link to="/login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ textDecoration: "none" }}>Login</Link>
           &nbsp;&nbsp;
-          <button type="button" onclick="location.href='../src/components/Login/Signup.js'" href="/signup" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</button>
-          
+          <Link to="/signup" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ textDecoration: "none" }}>Signup</Link>
         </div>
         <button data-collapse-toggle="navbar-dropdown" type="button" class="bg-warning inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown"
           aria-expanded="false"
@@ -89,114 +87,11 @@ function NavBar() {
               <a href="/guide" style={{ textDecoration: "none" }} class="flex block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
               <RiSurveyLine style={{ marginRight: "2px" }} />Guide</a>
             </li>
-            {/* <li>
-              <a href="/login" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
-            </li>
-            <li>
-              <a href="/signup" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Signup</a>
-            </li> */}
           </ul>
         </div>
       </div>
     </nav>
-    // <Navbar
-    //   expanded={expand}
-    //   fixed="top"
-    //   expand="md"
-    //   className={navColour ? "sticky" : "navbar"}
-    // >
-    //   <Container>
-    //     <Navbar.Brand href="/" className="d-flex">
-    //       <img src={logo} className="img-fluid logo" alt="brand" />
-    //       <span style={{color:"black"}}><b>KaKaQ</b></span>
-    //     </Navbar.Brand>
-    //     <Navbar.Toggle
-    //       aria-controls="responsive-navbar-nav"
-    //       onClick={() => {
-    //         updateExpanded(expand ? false : "expanded");
-    //       }}
-    //     >
-    //       <span></span>
-    //       <span></span>
-    //       <span></span>
-    //     </Navbar.Toggle>
-    //     <Navbar.Collapse id="responsive-navbar-nav">
-    //       <Nav className="ms-auto" defaultActiveKey="#home">
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/"
-    //             onClick={() => updateExpanded(false)}>
-    //             <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-    //           </Nav.Link>
-    //         </Nav.Item>
-
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/survey"
-    //             data-bs-toggle="dropdown" aria-expanded="false"
-    //             onClick={() => updateExpanded(false)}>
-    //             <AiOutlineUser style={{ marginBottom: "2px" }} /> Survey
-    //           </Nav.Link>
-    //             <li><a class="dropdown-item" href="/survey">Basic</a></li>
-    //             <li><a class="dropdown-item" href="/chatbot">ChatBot</a></li>
-    //             <li><a class="dropdown-item" href="/opensurvey">List</a></li>
-    //         </Nav.Item>
-
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/test"
-    //             onClick={() => updateExpanded(false)}
-    //           >
-    //             <CgFileDocument style={{ marginBottom: "2px" }} /> Test
-    //           </Nav.Link>
-    //         </Nav.Item>
-
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/mypage"
-    //             onClick={() => updateExpanded(false)}
-    //           >
-    //             <ImBlog style={{ marginBottom: "2px" }} /> MyPage
-    //           </Nav.Link>
-    //         </Nav.Item>
-
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/guide"
-    //             onClick={() => updateExpanded(false)}
-    //           >
-    //             <RiSurveyLine style={{ marginBottom: "2px" }} /> Guide
-    //           </Nav.Link>
-    //         </Nav.Item>
-
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/login"
-    //             onClick={() => updateExpanded(false)}
-    //           >
-    //             <RiSurveyLine style={{ marginBottom: "2px" }} /> Login
-    //           </Nav.Link>
-    //         </Nav.Item>
-
-    //         <Nav.Item>
-    //           <Nav.Link
-    //             as={Link}
-    //             to="/signup"
-    //             onClick={() => updateExpanded(false)}
-    //           >
-    //             <RiSurveyLine style={{ marginBottom: "2px" }} /> Signup
-    //           </Nav.Link>
-    //         </Nav.Item>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
+    
   );
 }
 
