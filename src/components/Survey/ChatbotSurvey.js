31740cc8c542f752cb8140547d5b4b240e7ef58d
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-04-28 12:47:58 +0900

    feat: creat mypage ui

--- src/components/Survey/ChatbotSurvey.js
+++ src/components/Survey/ChatbotSurvey.js
@@ -59,7 +59,6 @@ function ChatbotSurvey(props) {
     );
   });
 
-
   return (
     <Container fluid className="survey-header" >
       <Container class="flex items-center">
@@ -78,9 +77,7 @@ function ChatbotSurvey(props) {
           <div>
             {questionsAndAnswers}
           </div>
-
           <button type="submit" class="btn btn-primary" onClick={handleSubmit}>Make Survey</button><br />
-          {/* <h2 style={{ color: "white" }}>{message}</h2> */}
         </form>
       </Container>
     </Container>
@@ -89,136 +86,3 @@ function ChatbotSurvey(props) {
 
 export default ChatbotSurvey;
 
-
-{/* <Card className="survey-card-view ">
-<Card.Body>
-  <Question question="1. Type your full name" />
-  <RadioGroup>
-    <Radio name="gender" value="Man" defaultChecked>
-      Man
-    </Radio>
-    <br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-  </RadioGroup>
-</Card.Body>
-</Card>
-
-<Card className="survey-card-view">
-<Card.Body>
-  <Question question="2. Type your gender" />
-  <RadioGroup>
-    <Radio name="gender" value="Man" defaultChecked>
-      Man
-    </Radio>
-    <br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-    <Radio name="gender" value="Woman">
-      Woman
-    </Radio><br />
-  </RadioGroup>
-  <br />
-</Card.Body>
-</Card>
-
-<Card className="survey-card-view">
-<Card.Body>
-  <Question question="3. Type your age group" />
-  <RadioGroup>
-    <Radio name="ageGroup" value="20s" defaultChecked>
-      20s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="30s">
-      30s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="40s">
-      40s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="50s">
-      50s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="60s">
-      60s
-    </Radio>
-  </RadioGroup>
-  <br />
-</Card.Body>
-</Card>
-
-<Card className="survey-card-view">
-<Card.Body>
-  <Question question="4. Type your email address" />
-  <RadioGroup>
-    <Radio name="ageGroup" value="20s" defaultChecked>
-      20s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="30s">
-      30s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="40s">
-      40s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="50s">
-      50s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="60s">
-      60s
-    </Radio>
-  </RadioGroup>
-  <br />
-</Card.Body>
-</Card>
-
-<Card className="survey-card-view">
-<Card.Body>
-  <Question question="5. Type your address" />
-  <RadioGroup>
-    <Radio name="ageGroup" value="20s" defaultChecked>
-      20s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="30s">
-      30s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="40s">
-      40s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="50s">
-      50s
-    </Radio>
-    <br />
-    <Radio name="ageGroup" value="60s">
-      60s
-    </Radio>
-  </RadioGroup>
-  <br />
-</Card.Body>
-</Card> */}
