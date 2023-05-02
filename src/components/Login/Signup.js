c4cf7b8b1642e1289da94c7d49e7e5c3cb4d7f44
001021 <84309420+001021@users.noreply.github.com>
2023-05-02 14:56:05 +0900

    EDIT signup
    signup design 수정

--- src/components/Login/Signup.js
+++ src/components/Login/Signup.js
@@ -99,86 +99,96 @@ function SignupForm(props) {
 
     return (
         <Container fluid className="signup-header mx-md" class="flex flex-col items-center justify-center px-2 py-2 ">
-            <form onSubmit={handleSubmit}>
-                <div class="p-20 px-10 space-y-4 md:space-y-6 sm:p-8" style={{ alignItems: "center" }}>
-                    <div class="w-auto md:w-auto isolate px-36 py-2 sm:py-32 lg:px-8">
-                        <div class="mx-auto max-w-2xl text-center">
-                            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">회원가입</h2>
-                            <p class="mt-2 text-lg leading-8 text-gray-600">KaKaQ에 가입하여 여러 편리한 설문조사 기능을 이용해보세요.</p>
-                        </div>
-                        <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                            <div class="sm:col-span-2">
-                                <label for="username" class="block text-sm font-semibold leading-6 text-gray-900">사용자 이름</label>
-                                <div class="mt-2.5">
-                                    <input type="text"
-                                        id="username"
-                                        name="username"
-                                        value={formData.username}
-                                        onChange={nameInputChange}
-                                        class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="username" required />
-                                </div>
+            <Container fluid className="signup-header mx-md" class="flex flex-col items-center justify-center px-2 py-2 " style={{ maxWidth: "830px" }}>
+                <form onSubmit={handleSubmit}>
+                    <div class="p-20 px-10 space-y-4 md:space-y-6 sm:p-8" style={{ alignItems: "center" }}>
+                        <div class="w-auto md:w-auto isolate px-36 py-2 sm:py-32 lg:px-8">
+                            <div class="mx-auto max-w-2xl text-center">
+                                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">회원가입</h2>
+                                <p class="mt-2 text-lg leading-8 text-gray-600">KaKaQ에 가입하여 여러 편리한 설문조사 기능을 이용해보세요.</p>
                             </div>
-                            <div class="sm:col-span-2" style={{ alignItems: "center" }}>
-                                <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">이메일</label>
-                                <div class="mt-2.5">
-                                    <input type="text"
-                                        id="email"
-                                        name="email"
-                                        value={formData.email}
-                                        onChange={mailInputChange}
-                                        class="block leading-3 w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="email" required />
+                            <br />
+                            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                                <div class="sm:col-span-2">
+                                    <div class="flex flex-col sm:flex-row items-center">
+                                        <label for="username" class="block text-sm font-semibold leading-6 text-gray-900 sm:w-1/4 mb-2 sm:mb-0 sm:mr-4 sm:text-right">사용자 이름</label>
+                                        <div class="flex-1">
+                                            <input type="text"
+                                                id="username"
+                                                name="username"
+                                                value={formData.username}
+                                                onChange={nameInputChange}
+                                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                placeholder="username" required />
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="sm:col-span-2">
-                                <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">비밀번호</label>
-                                <div class="mt-2.5">
-                                    <input type="password"
-                                        id="password"
-                                        name="password"
-                                        value={formData.password}
-                                        onChange={pwdInputChange}
-                                        class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="password" required />
+                                <div class="sm:col-span-2">
+                                <div class="flex flex-col sm:flex-row items-center">
+                                    <label for="company" class="block text-sm font-semibold leading-6 text-gray-900 sm:w-1/4 mb-2 sm:mb-0 sm:mr-4 sm:text-right">이메일</label>
+                                    <div class="flex-1">
+                                        <input type="text"
+                                            id="email"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={mailInputChange}
+                                            class="block leading-3 w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            placeholder="email" required />
+                                    </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="sm:col-span-2">
-                                <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">비밀번호 확인</label>
-                                <div class="mt-2.5">
-                                    <input type="password"
-                                        id="confirmPassword"
-                                        name="confirmPassword"
-                                        value={formData.confirmPassword}
-                                        onChange={cfpwdInputChange}
-                                        class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="comfirmpassword" required />
+                                <div class="sm:col-span-2">
+                                <div class="flex flex-col sm:flex-row items-center">
+                                    <label for="company" class="block text-sm font-semibold leading-6 text-gray-900 sm:w-1/4 mb-2 sm:mb-0 sm:mr-4 sm:text-right">비밀번호</label>
+                                    <div class="flex-1">
+                                        <input type="password"
+                                            id="password"
+                                            name="password"
+                                            value={formData.password}
+                                            onChange={pwdInputChange}
+                                            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            placeholder="password" required />
+                                    </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="flex gap-x-4 sm:col-span-2">
-                                <div class="flex h-6 items-center">
-                                    <button type="submit"
-                                        disabled={!(isData.username && isData.email && isData.password && isData.confirmPassword)}
-                                        class="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">
-                                        <span class="sr-only">Agree to policies</span>
+                                <div class="sm:col-span-2">
+                                <div class="flex flex-col sm:flex-row items-center">
+                                    <label for="email" class="block text-sm font-semibold leading-6 text-gray-900 sm:w-1/4 mb-2 sm:mb-0 sm:mr-4 sm:text-right">비밀번호 확인</label>
+                                    <div class="flex-1">
+                                        <input type="password"
+                                            id="confirmPassword"
+                                            name="confirmPassword"
+                                            value={formData.confirmPassword}
+                                            onChange={cfpwdInputChange}
+                                            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            placeholder="comfirm password" required />
+                                    </div>
+                                    </div>
+                                </div>
+                                <div class="flex gap-x-4 sm:col-span-2">
+                                    <div class="flex h-6 items-center">
+                                        <button type="submit"
+                                            disabled={!(isData.username && isData.email && isData.password && isData.confirmPassword)}
+                                            class="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">
+                                            <span class="sr-only">Agree to policies</span>
 
-                                        <span aria-hidden="true" class="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
-                                    </button>
+                                            <span aria-hidden="true" class="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
+                                        </button>
+                                    </div>
+                                    <label class="text-sm leading-6 text-gray-600" id="switch-1-label">
+                                        By selecting this, you agree to our
+                                        <a href="#" class="font-semibold text-indigo-600">&nbsp;privacy&nbsp;policy</a>.
+                                    </label>
                                 </div>
-                                <label class="text-sm leading-6 text-gray-600" id="switch-1-label">
-                                    By selecting this, you agree to our
-                                    <a href="#" class="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
-                                </label>
                             </div>
-                        </div>
-                        <div class="mt-10">
-                            <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${!(isData.username && isData.email && isData.password && isData.confirmPassword) ? 'disabled' : ''}"
-                                style={{ backgroundColor: !(isData.username && isData.email && isData.password && isData.confirmPassword) ? '#ccc' : '' }}>회원가입</button>
+                            <div class="mt-10">
+                                <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${!(isData.username && isData.email && isData.password && isData.confirmPassword) ? 'disabled' : ''}"
+                                    style={{ backgroundColor: !(isData.username && isData.email && isData.password && isData.confirmPassword) ? '#ccc' : '' }}>회원가입</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {/* ============= */}
-                {/* <div class="isolate px-6 py-2 sm:py-32 lg:px-8">
+                    {/* ============= */}
+                    {/* <div class="isolate px-6 py-2 sm:py-32 lg:px-8">
                     <div class="mx-auto max-w-2xl text-center">
                         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">회원가입</h2>
                         <p class="ml-2 text-lg leading-8 text-gray-600">KaKaQ에 회원 가입하여 여러 편리한 설문조사 기능을 이용해보세요!</p>
@@ -251,8 +261,9 @@ function SignupForm(props) {
                         회원가입
                     </button>  
                 </div> */}
-            </form>
-            <br />
+                </form>
+                <br />
+            </Container>
         </Container>
     );
 }
