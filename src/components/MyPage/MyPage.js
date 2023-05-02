5abeb9047c9763517f881c1638ad2e41c7f2de84
jykyeong99 <2009skylove@naver.com>
2023-05-02 15:40:48 +0900

    Conflict resolve

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -1,4 +1,3 @@
-import React , { useState,useEffect }from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import TimelineCard from "./MyPageCards";
 import Particle from "../Particle";
