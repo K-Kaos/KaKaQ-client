1f5af7ac559b50dd27cae8fe75022e406da4eced
001021 <84309420+001021@users.noreply.github.com>
2023-05-02 09:59:31 +0900

    UI 개선
    EDIT Navbar Logout button (Why am i crying...)
    EDIT font-style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -115,9 +115,9 @@ function NavBar(props) {
             </li>
             {/* isLogin 값이 참이면 Logout 버튼만 / 거짓이면 Login, Signup */}
             {isLogin ?
-              <li class="md:order-2 text-2xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ textDecoration: "none" }}>
+              <li>
                 {/* <NavButton to="/signup" text="Signup" /> */}
-                <button onClick={handleLogout}>Logout</button>
+                <button onClick={handleLogout} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-4 py-2 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style={{ textDecoration: "none" }}>Logout</button>
               </li>
               :
               <>
