d08d9a6333016603b46174ccb4913dc946af370c
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-05-03 11:55:20 +0900

    design: edit button ui

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -72,11 +72,9 @@ function LoginForm(props) {
             <section class="bg-gray-50 dark:bg-gray-900">
                 <div
                     class="flex flex-col items-center justify-center px-4 py-4 mx-auto md:h-screen lg:py-0"
-                    style={{ background: "#fffacd" }}
-                >
+                    style={{ background: "#fffacd" }}>
                     <div
-                        class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-                    >
+                        class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1
                                 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
