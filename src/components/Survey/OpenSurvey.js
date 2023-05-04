ee761af02db25c80fe31fb35e59ed2d472d4483b
jykyeong99 <2009skylove@naver.com>
2023-05-04 11:23:36 +0900

    openSurvey List

--- src/components/Survey/OpenSurvey.js
+++ src/components/Survey/OpenSurvey.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import OpenSurveyItem from './OpenSurveyItem';
 import pizza_image from '../../Assets/Images/Pizza.jpg'
 import museum_image from '../../Assets/Images/Museum.jpg'
@@ -7,65 +7,50 @@ import hotel_image from '../../Assets/Images/hotel.jpg'
 import tang_image from '../../Assets/Images/tang.jpg'
 import seobin from "../../Assets/profile/seobin.png";
 import './OpenSurvey.css'
+import axios from "axios";
 
 function OpenSurvey() {
     let whoLoggedIn = null;
+    const [openSurveys, setOpenSurveys] = useState([]);
+
     useEffect(() => {
         whoLoggedIn = sessionStorage.getItem('whoLoggedIn');
         if (whoLoggedIn === null) {
             alert("로그인 후 이용해 주세요");
             window.location.href = "/login";
         }
+        axios.get("/api/surveys", {
+          }).then(function (response) {
+            const surveys = response.data;
+            setOpenSurveys(surveys);
+            // for (i=0;i<surveys.length;i++){
+            //     CreateSurveyItem(surveys[i]);
+            // }
+          }).catch(function (error) {
+            console.log(error);
+          });
     }, []);
+
+    // function CreateSurveyItem(){
+
+
+    // }
     return (
         <div className='surveys' class="py-24 sm:py-32">
             <h1>이 지역에서만 할 수 있는 설문조사에요!</h1>
             <div className='surveys__container' class="mx-auto max-w-7xl px-6 lg:px-8">
                 <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    <OpenSurveyItem
-                        date="2023/07/09"
-                        text="우리집 피자 어땠나요? 당신의 의견을 들려주세요!" // 설문 간단 설명
-                        label="MyhomePizza" // 설문 카테고리
+                    {openSurveys.map((survey) => (
+                        <OpenSurveyItem
+                        startdate={survey.startDate}
+                        enddate={survey.endDate}
+                        text={survey.title} // 설문 제목
+                        label={survey.keyword} // 설문 카테고리
                         path="/Home" // 설문 참여 창으로
-                        name="Seobin Lee"
+                        name={survey.creator.username}
                         profile={seobin}
-                    />
-
-                    <OpenSurveyItem
-                        date="2023/07/09"
-                        text="국립공주박물관에 오신 것을 환영합니다." // 설문 간단 설명
-                        label="PrincessMuseum" // 설문 카테고리
-                        path="" // 설문 참여 창으로
-                        name="Seobin Lee"
-                        profile={seobin}
-                    />
-                    <OpenSurveyItem
-                        date="2023/07/09"
-                        text="5분만 시간 내주세요..." // 설문 간단 설명
-                        label="Please" // 설문 카테고리
-                        path="" // 설문 참여 창으로
-                        name="Seobin Lee"
-                        profile={seobin}
-                    />
-
-                    <OpenSurveyItem
-                        date="2023/07/09"
-                        text="뜨끈한 국물이 땡기는 오늘, 도리도리에서 닭볶음탕 한 사바리 어떠세요? 설문조사 후 방문하시면 음료수 서비스를 드립니다!" // 설문 간단 설명
-                        label="doridori" // 설문 카테고리
-                        path="" // 설문 참여 창으로
-                        name="Seobin Lee"
-                        profile={seobin}
-                    />
-
-
-                    <OpenSurveyItem
-                        date="2023/07/09"
-                        text="호텔에서 편안한 여행을 보내셨나요?" // 설문 간단 설명
-                        label="hotel" // 설문 제목
-                        path="" // 설문 참여 창으로
-                        name="Seobin Lee"
-                        profile={seobin}
-                    />
+                        />
+                    ))}
                 </div>
             </div>
         </div>
