51b8acac7be3c51b7d2d77dbccdbd7e42c1ee21b
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-05-20 23:57:30 +0900

    feat: add workspace

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import Survey from "./components/Survey/Survey";
 import Test from "./components/Test/Test";
 import MyPage from "./components/MyPage/MyPage";
+import Workspace from "./components/MyPage/Workspace";
 import Guide from "./components/Guide/Guide";
 import OpenSurvey from "./components/Survey/OpenSurvey"
 import Login from "./components/Login/Login"
@@ -64,6 +65,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" />} />
           <Route path="/surveycompletion" element={<SurveyCompletion />} />
           <Route path="/kakaq" element={<KAKAQ />} />
+          <Route path="/workspace" element={<Workspace />} />
         </Routes>
       </div>
     </Router>
