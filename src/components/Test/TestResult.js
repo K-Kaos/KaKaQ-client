f5b1dee59e9d037aca44219d1095cfed145b96cf
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-06-03 23:21:35 +0900

    refactor: edit style

--- src/components/Test/TestResult.js
+++ src/components/Test/TestResult.js
@@ -25,17 +25,31 @@ function TestResult(props) {
     <div class="MuiContainer-root MuiContainer-maxWidthLg jss7 css-okglv3">
       <div tabindex="-1" style={{ outline: "none" }}>
         <div class="MuiContainer-root MuiContainer-maxWidthLg css-1qtp0is">
-          <div class="MuiBox-root css-ip3vow">
+          <div class="MuiBox-root css-ip3vow" style={{ display: "block", textAlign: "-webkit-center" }}>
             <div className="MuiBox-root css-0">
-              <div className="MuiGrid-root MuiGrid-container css-sow6z2">
-              <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-md-12 MuiGrid-grid-lg-12 css-we0np1">
+              <div
+                className="MuiGrid-root MuiGrid-container css-sow6z2"
+                style={{ display: "block", textAlign: "-webkit-center" }}
+              >
+                <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-md-12 MuiGrid-grid-lg-12 css-we0np1">
                   <div className="MuiBox-root css-0">
                     <p className="MuiTypography-root MuiTypography-body1 css-zuj73s">
                       <span style={{ color: "rgb(250, 200, 0)" }}>
                         {props.surveyTitle}{" "}
                       </span>
-                      <img src="https://t1.kakaocdn.net/friends/new_store/prod/character/character_20230502133540_5b2ce511c22d4197807e32485d3fca0c.png">
-                        </img></p>
+                      <div
+                        style={{
+                          width: "123px",
+                          height: "123px",
+                          borderRadius: "50%",
+                          overflow: "hidden",
+                          margin: "0 auto",
+                          boxShadow: "0 10px 25px 0 rgba(0,0,0,.3)",
+                        }}
+                      >
+                        <img src="https://t1.kakaocdn.net/friends/new_store/prod/character/character_20230502133540_5b2ce511c22d4197807e32485d3fca0c.png"></img>
+                      </div>
+                    </p>
                   </div>
                 </div>
                 <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-md-12 MuiGrid-grid-lg-12 css-we0np1">
@@ -44,16 +58,23 @@ function TestResult(props) {
                       <span style={{ color: "rgb(250, 200, 0)" }}>
                         {props.surveyTitle}{" "}
                       </span>
-                      당신은 {result}입니다!
+                      당신의 캐릭터는 {result}입니다!
                     </p>
                   </div>
                 </div>
-                <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-md-8 MuiGrid-grid-lg-12 css-1iqtsvr">
-                  <div className="MuiBox-root css-bdnmmq">
-                    <p className="MuiTypography-root MuiTypography-body1 css-qisfzi">
-                      //
-                    </p>
-                    <div className="MuiBox-root css-149sczi">
+                <div class="MuiGrid-root" style={{ textAlign: "-webkit-center" }}>
+                  <div
+                    className="MuiGrid-root MuiGrid-item MuiGrid-grid-md-8 MuiGrid-grid-lg-12 css-1iqtsvr"
+                    style={{ maxWidth: "70%",textAlign: "-webkit-center" }}
+                  >
+                    <div
+                      className="MuiBox-root css-bdnmmq mb-4"
+                      style={{ placeContent: "center" }}
+                    >
+                      <p className="MuiTypography-root MuiTypography-body1 css-qisfzi">
+                        //
+                      </p>
+                      {/* <div className="MuiBox-root css-149sczi">
                       <button
                         className="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk"
                         tabIndex="0"
@@ -86,12 +107,17 @@ function TestResult(props) {
                         </svg>
                         <span className="MuiTouchRipple-root css-w0pj6f"></span>
                       </button>
+                    </div> */}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="MuiGrid2-root MuiGrid2-container MuiGrid2-direction-xs-row css-9329ze">
+
+            <div
+              class="MuiGrid2-root MuiGrid2-container MuiGrid2-direcxtion-xs-row css-9329ze"
+              style={{ maxWidth: "70%" }}
+            >
               <div class="MuiGrid2-root MuiGrid2-direction-xs-row MuiGrid2-grid-xs-true css-1hoyqw5">
                 <div class="ShareProjectSettings__ShareContentCard-sc-1713g9x-0 kpxJWt MuiBox-root css-1po3p4o">
                   <div className="MuiBox-root css-ldo4d5">
@@ -103,8 +129,12 @@ function TestResult(props) {
                         onClick={handleCreateSurveyClick}
                       >
                         <span className="MuiButton-startIcon MuiButton-iconSizeMedium css-6xugel">
-                        <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" 
-                        viewBox="0 0 24 24"><path d="M11 15H17V17H11V15M9 7H7V9H9V7M11 13H17V11H11V13M11 9H17V7H11V9M9 11H7V13H9V11M21 5V19C21 20.1 20.1 21 19 21H5C3.9 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H19C20.1 3 21 3.9 21 5M19 5H5V19H19V5M9 15H7V17H9V15Z" /></svg>
+                          <svg
+                            className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M11 15H17V17H11V15M9 7H7V9H9V7M11 13H17V11H11V13M11 9H17V7H11V9M9 11H7V13H9V11M21 5V19C21 20.1 20.1 21 19 21H5C3.9 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H19C20.1 3 21 3.9 21 5M19 5H5V19H19V5M9 15H7V17H9V15Z" />
+                          </svg>
                         </span>
                         <p className="MuiTypography-root MuiTypography-body1 css-awn6hr">
                           새로운 설문조사 만들기
@@ -118,7 +148,12 @@ function TestResult(props) {
                         onClick={handleClick}
                       >
                         <span className="MuiButton-startIcon MuiButton-iconSizeMedium css-6xugel">
-                        <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" viewBox="0 0 24 24"><path d="M7 4C4.8 4 3 5.8 3 8S4.8 12 7 12 11 10.2 11 8 9.2 4 7 4M7 10C5.9 10 5 9.1 5 8S5.9 6 7 6 9 6.9 9 8 8.1 10 7 10M0 18C0 15.8 3.1 14 7 14C8.5 14 9.9 14.3 11 14.7V17C10.2 16.5 8.8 16 7 16C3.8 16 2 17.4 2 18H11V20H0V18M22 4H15C13.9 4 13 4.9 13 6V18C13 19.1 13.9 20 15 20H22C23.1 20 24 19.1 24 18V6C24 4.9 23.1 4 22 4M22 18H15V6H22V18Z" /></svg>
+                          <svg
+                            className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M7 4C4.8 4 3 5.8 3 8S4.8 12 7 12 11 10.2 11 8 9.2 4 7 4M7 10C5.9 10 5 9.1 5 8S5.9 6 7 6 9 6.9 9 8 8.1 10 7 10M0 18C0 15.8 3.1 14 7 14C8.5 14 9.9 14.3 11 14.7V17C10.2 16.5 8.8 16 7 16C3.8 16 2 17.4 2 18H11V20H0V18M22 4H15C13.9 4 13 4.9 13 6V18C13 19.1 13.9 20 15 20H22C23.1 20 24 19.1 24 18V6C24 4.9 23.1 4 22 4M22 18H15V6H22V18Z" />
+                          </svg>
                         </span>
                         <p className="MuiTypography-root MuiTypography-body1 css-awn6hr">
                           나의 워크스페이스 보기
