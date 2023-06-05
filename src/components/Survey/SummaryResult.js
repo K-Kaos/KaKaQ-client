68081ca65ad772280b2db37ea857bf4c22be8978
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-06-05 14:15:52 +0900

    feat: add navbar in result page

--- src/components/Survey/SummaryResult.js
+++ src/components/Survey/SummaryResult.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import BarChart from "./BarChart";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-
+import logo from "../../Assets/Logo/logo.png";
 
 
 function SummaryResult() { 
@@ -18,6 +18,7 @@ function SummaryResult() {
     const [surveyCreator, setSurveyCreator] = useState([]);
     const [city, setCity] = useState([]);
     const [users, setUsers] = useState([]);
+    const [showProfile, setShowProfile] = useState(false);
 
     useEffect(() => {
         const LoggedInUser = sessionStorage.getItem('whoLoggedIn');
@@ -81,9 +82,127 @@ function SummaryResult() {
         setCurrentDate(nextMonth);
     };
 
+    const handleClickProfile = () => {
+        setShowProfile(true);
+      };
+    
+      const handleCloseProfile = () => {
+        setShowProfile(false);
+      };
+      
     return (
         <>
             <div class="MuiBox-root css-u244mv">
+            <header
+              class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation4 MuiAppBar-root MuiAppBar-colorPrimary MuiAppBar-positionFixed mui-fixed css-1jrh10h"
+              style={{
+                boxShadow: "none",
+                borderBottomWidth: "2px",
+                borderBottomStyle: "solid",
+                borderBottomColor: "rgb(238, 238, 238)",
+              }}
+            >
+              <div
+                class="MuiToolbar-root MuiToolbar-gutters MuiToolbar-regular css-i6s8oy"
+                style={{
+                  height: "100%",
+                  minHeight: "56px",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div
+                  class="MuiBox-root css-0"
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexGrow: "1",
+                    flexShrink: "1",
+                    flexBasis: "0%",
+                  }}
+                >
+                  <div
+                    class="MuiBox-root css-12kkxif"
+                    aria-label="Back to dashboard"
+                  >
+                    <img
+                      src={logo}
+                      alt="KAKAQ-logo"
+                      style={{ height: "18px" }}
+                    />
+                  </div>
+                  <div class="MuiBox-root css-0">
+                    <div
+                      class="MuiBox-root css-0"
+                      style={{
+                        display: "flex",
+                        paddingLeft: "20px",
+                        alignItems: "center",
+                        top: "18px",
+                        position: "absolute",
+                        flexDirection: "row",
+                      }}
+                    >
+                      <div class="MuiBox-root css-191gqa8">
+                        <p class="MuiTypography-root MuiTypography-body1 css-qt1p9i">
+                          내 프로젝트
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="MuiBox-root css-0"></div>
+                </div>
+                <div class="MuiBox-root css-tzmmb1">
+                  <div class="MuiBox-root css-1gmwkzf">
+                    <a
+                      class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk"
+                      tabindex="0"
+                      href="가이드"
+                      target="_blank"
+                      aria-label="프로젝트 가이드 보러가기"
+                    >
+                      <svg
+                        class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-8feus5"
+                        focusable="false"
+                        aria-hidden="true"
+                        viewBox="0 0 24 24"
+                        data-testid="HelpRoundedIcon"
+                      >
+                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75-.9.92c-.5.51-.86.97-1.04 1.69-.08.32-.13.68-.13 1.14h-2v-.5c0-.46.08-.9.22-1.31.2-.58.53-1.1.95-1.52l1.24-1.26c.46-.44.68-1.1.55-1.8-.13-.72-.69-1.33-1.39-1.53-1.11-.31-2.14.32-2.47 1.27-.12.37-.43.65-.82.65h-.3C8.4 9 8 8.44 8.16 7.88c.43-1.47 1.68-2.59 3.23-2.83 1.52-.24 2.97.55 3.87 1.8 1.18 1.63.83 3.38-.19 4.4z"></path>
+                      </svg>
+                      <span class="MuiTouchRipple-root css-w0pj6f"></span>
+                    </a>
+                  </div>
+                  <button
+                    class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-148fdm8"
+                    tabindex="0"
+                    type="button"
+                    aria-label="설정"
+                    style={{
+                      width: "32px",
+                      height: "32px",
+                      borderRadius: "16px",
+                      overflow: "hidden",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <div
+                      class="MuiAvatar-root MuiAvatar-circular css-1kavoh2"
+                      style={{ width: "100%", height: "100%" }}
+                    >
+                      <img
+                        alt="profile_image"
+                        src="https://lh3.googleusercontent.com/a/AGNmyxYCL1L24exss9DNiVXadg4hyk22I6p-ygUS50-mVx0=s96-c"
+                        class="MuiAvatar-img css-1hy9t21"
+                        onClick={handleClickProfile}
+                      />
+                    </div>
+                    <span class="MuiTouchRipple-root css-w0pj6f"></span>
+                  </button>
+                </div>
+              </div>
+            </header>
                 <div
                     class="MuiTabPanel-root css-1pyy37n"
                     role="tabpanel"
